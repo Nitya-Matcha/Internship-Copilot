174a8268f5beac9f012f09398e574ce83974101e
Nitya Matcha <nitya.matcha@gmail.com>
2026-07-23 12:06:10 -0400

    CORS fix

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,8 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173"
+        "http://localhost:5173",
+        "https://internship-copilot-test1-nc412kb74-nitya5.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
